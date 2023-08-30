d26cebd93992f9bb838e8e3d9fa9699ec01380df
Hovo-Alaverdyan <hovo.alaverdyan05@gmail.com>
2023-08-30 16:16:01 +0400

    "HomePage-Media"

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export function HomePage() {
   return (
-    <div className=' flex justify-center items-center h-[100vh] flex-col'>
+    <div className=' flex justify-center items-center h-[100vh] flex-col text-center'>
       <h1 className=' text-white text-[3rem] font-bold'>Ready to watch? Let's Start!</h1>
       <Link to='/shows' className=" flex justify-center items-center  bg-red-600 rounded-full p-3 gap-3 cursor-pointer">
         <button className=' text-white text-2xl'>Get Started</button>
